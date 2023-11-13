008f071f1a2e13861b77a0a7c0d23734ea327efb
Dukeman330 <cgarson@gmail.com>
2023-11-13 15:55:30 -0500

    updated client app

--- client/src/App.js
+++ client/src/App.js
@@ -15,9 +15,8 @@ function App() {
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+            rel="noopener noreferrer">
+            Learn React, now with KUBERNETES!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
